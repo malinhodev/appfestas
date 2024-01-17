90c024b820ea671e71711b7791357d2618f08dbf
malinhodev <malinhodev@gmail.com>
2024-01-17 10:48:10 -0300

    rota de usuario finalizada

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,16 @@ const bcrypt = require('bcrypt')
 
 const User = require('../models/user')
 //get an user
-router.get('/', async(req,res)=>{
-    res.json({msg:'funcionando!'})
+router.get('/:id', async(req,res)=>{
+    const id = req.params.id
+    //verify user
+    try{
+        const user = await User.findOne({_id:id}, {password:0})
+        res.json({error:null,user})
+    }catch(err){
+        return res.status(400).json({error:'Usuário não existe!'})
+    }
+    
 })
 
 module.exports = router
