3563aae83282e1ff2c43076fb72fc383aad14266
malinhodev <malinhodev@gmail.com>
2024-01-17 14:35:28 -0300

    iniciando rota de update

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 //middlewares
 const verifyToken = require('../helpers/check-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 //get an user
 router.get('/:id', verifyToken, async(req,res)=>{
     const id = req.params.id
@@ -18,7 +19,22 @@ router.get('/:id', verifyToken, async(req,res)=>{
 })
 //update an user
 router.put('/', verifyToken, async (req,res)=>{
+    const token = req.header('auth-token')
+    const user = await getUserByToken(token)
+    const userReqId = req.body.id
+    const password = req.body.password
+    const confirmpassword= req.body.confirmpassword
 
+    const userId = user._id.toString()
+    //check if use is iqual token user id
+    if(userId != userReqId){
+        res.status(401).json({error: 'Acesso negado!'})
+    }
+    //create an user object
+    const updateData = {
+        name: req.body.name,
+        email: req.body.email
+    }
 })
 
 module.exports = router
